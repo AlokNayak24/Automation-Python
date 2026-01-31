7adbdd8ec6918028aec09f76e33767b8f18ed78e
a.nayak <aloknayak2013@gmail.com>
2026-01-31 04:53:42 -0800

    Third Commit

---
+++ Day 2/Operators.py
@@ -0,0 +1,35 @@
+# Arithmetic Operator
+from operator import truediv
+
+a=10
+b=4
+print(a+b) #addition
+print(a-b) #substraction
+print(a*b) #Multification
+
+
+print(a/b)  # 2.5
+print(a//b) # 2 Quotient
+print(a%b)  # 1 Reminder
+print(a**b) # 10**4
+
+#Relation operator
+# > < >= <= !=
+a=5
+b=20
+
+print(a>b) #False
+print(a<b) #True
+print(a==b) #False
+print(a!=b) #True
+print(a>=b) #False
+print(a<=b) #True
+
+# Logical Operator
+# and or not
+a= True
+b= False
+print(a and b)
+print(a or b)
+print(not a)
+print(not b)
