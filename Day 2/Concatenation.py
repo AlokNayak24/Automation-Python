853ea0b3e2070e023a57799aad7011e77dfdc8d7
a.nayak <aloknayak2013@gmail.com>
2026-02-02 08:51:49 -0800

    Fourth Commit

---
+++ Day 2/Concatenation.py
@@ -0,0 +1,15 @@
+# concatenation
+
+print(10+10)        #20
+print(10.5+12.00)   #22.5
+print(10+10.5)      #20.5
+
+print('welcome'+'python')  #WelcomePython
+
+print(True+5)      #6
+print(False+5)     #5
+print(True+True)   #2
+
+print(10+'welcome')     # Not Valid Because 2 values are diff
+print(10.5+'welcome')   # Not Valid Because 2 values are diff
+print(True+'welcome')   # Not Valid Because 2 values are diff
